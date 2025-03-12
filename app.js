f244160417decf880ecf31988300e77da1aafb8a
ricky0601 <ricky2425@tukorea.ac.kr>
2025-03-12 18:12:16 +0900

    0312 modify commit

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ app.listen(port);
 
 const userRouter = require('./routes/users');
 const bookRouter = require('./routes/books');
+const categoryRouter = require('./routes/categories');
 const likeRouter = require('./routes/likes');
 const cartRouter = require('./routes/carts');
 const orderRouter = require('./routes/orders');
 
 app.use("/users", userRouter);
 app.use("/books", bookRouter);
+app.use("/category", categoryRouter);
 app.use("/likes", likeRouter);
 app.use("/carts", cartRouter);
 app.use("/orders", orderRouter);
--- routes/books.js
+++ routes/books.js
@@ -1,29 +1,9 @@
 const express = require('express');
 const router = express.Router();
 router.use(express.json());
+const { allBooks , bookDetail } = require("../controller/BookController");
 
-router.get("/",(req, res) => {
-    res.status(200).json({
-        message : "전제 도서 조회 성공"
-    })
-});
-
-router.get("/:bookId",(req, res) => {
-    const bookId = req.params.bookId;
-
-    res.status(200).json({
-        bookId : bookId,
-        message : "개별 도서 조회 성공"
-    })
-});
-
-router.get("/",(req, res) => {
-    const categoryId = req.query.categoryId;
-    const checkNew = req.query.new;
-
-    res.status(200).json({
-        message : "카테고리별 조회 성공"
-    })
-});
+router.get("/", allBooks);
+router.get("/:bookId", bookDetail);
 
 module.exports = router;
