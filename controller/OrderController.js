53b543d386f30aa8b06a8dbe6da5fd86b996ce8e
1dg <ricky2425@naver.com>
2025-03-20 23:18:01 +0900

    0320 Cart & Order modify

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,70 +1,102 @@
-const conn = require('../mariadb');
+// const conn = require('../mariadb');
+
+const mariadb = require('mysql2/promise');
 const {StatusCodes} = require('http-status-codes');
 
-const order = (req, res) => {
+const order = async (req, res) => {
+
+    const conn = await mariadb.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'root',
+        database: 'Bookshop',
+        port: 3306,
+        dateStrings: true,
+    });
+
     const {items, delivery, totalQuantity, totalPrice, userId, firstBookTitle} = req.body;
 
-    let delivery_id = 3;
-    let order_id = 1;
-
+    // delivery 테이블 넣기
     let sql = "INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)";
     let values = [delivery.address, delivery.receiver, delivery.contact];
 
-    // conn.query(sql, values, (err, results) => {
-    //     if(err){
-    //         console.log(err);
-    //         return res.status(StatusCodes.BAD_REQUEST).end();
-    //     }
+    let [results] = await conn.execute(sql, values);
 
-    //     delivery_id = results.insertId;
-    //     return res.status(StatusCodes.CREATED).json(results);
-    // })
+    let delivery_id = results.insertId;
 
+    // orders 테이블 넣기
     sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) 
                 VALUES (?, ?, ?, ?, ?);`
     values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
 
-    // conn.query(sql, values, (err, results) => {
-    //     if(err){
-    //         console.log(err);
-    //         return res.status(StatusCodes.BAD_REQUEST).end();
-    //     }
+    [results] = await conn.execute(sql, values);
+    let order_id = results.insertId;
 
-    //     order_id = results.insertId;
-    //     console.log(order_id);
+    // items 를 가지고 장바구니에서 book_id, quantity 꺼내기
+    sql = `SELECT book_id, quantity FROM cartItems WHERE id IN (?)`;
+    let [orderItems, fields] = await conn.query(sql, [items]);
 
-    //     return res.status(StatusCodes.CREATED).json(results);
-    // })
-
+    // orderedBook 데이터 넣기
     sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?;`;
 
     values = [];
-    items.forEach((item) => {
+    orderItems.forEach((item) => {
         values.push([order_id, item.book_id, item.quantity]);
     });
 
-    conn.query(sql, [values], (err, results) => {
-        if(err){
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
-        return res.status(StatusCodes.CREATED).json(results);
-    })
+    results = await conn.query(sql, [values]);
+
+    let result = await deleteCartItems(conn, [items]);
+
+    return res.status(StatusCodes.OK).json(result);
 };
 
-const getOrders = (req, res) => {
-    res.status(200).json({
-        message : "주문 목록(내역) 조회 성공"
-    })
+const deleteCartItems = async (conn, items) => {
+    let sql = `DELETE FROM cartItems WHERE id IN (?)`;
+
+    let result = await conn.query(sql, items);
+    return result;
+}
+
+const getOrders = async (req, res) => {
+
+    const conn = await mariadb.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'root',
+        database: 'Bookshop',
+        port: 3306,
+        dateStrings: true,
+    });
+
+    let sql = `SELECT orders.id, created_at, address, receiver, contact,
+                book_title, total_quantity, total_price
+                FROM orders LEFT JOIN delivery
+                ON orders.delivery_id = delivery_id;`;
+    let [rows, fields] = await conn.query(sql);
+    return res.status(StatusCodes.OK).json(rows);
+
 };
 
-const getOrderDetail = (req, res) => {
-    const orderId = req.params.orderId;
-    
-    res.status(200).json({
-        orderId : orderId,
-        message : "주문 상세 상품 조회 성공"
-    })
+const getOrderDetail = async (req, res) => {
+
+    const {orderId} = req.params;
+
+    const conn = await mariadb.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'root',
+        database: 'Bookshop',
+        port: 3306,
+        dateStrings: true,
+    });
+
+    let sql = `SELECT book_id, books.title AS book_title, author, price, quantity
+                FROM orderedBook LEFT JOIN books
+                ON orderedBook.book_id = books.id
+                WHERE order_id = ?;`;
+    let [rows, fields] = await conn.query(sql , [orderId]);
+    return res.status(StatusCodes.OK).json(rows);
 };
 
 module.exports = {
